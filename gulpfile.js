77dc70723e9f57533d46ae0694d93573010a92b0
Yatskevych Egor <yatskevychegor@hotmail.com>
2017-11-01 14:43:04 +0200

    gulp task refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -20,57 +20,7 @@ const reapiSmaParams = {
     dest: config.reapi.dest.pluginsDir
 };
 
-gulp.task('plugins', () => {
-    return gulp.src('./src/scripts/**/*.sma')
-        .pipe(sma(smaParams))
-        .pipe(flatten())
-        .pipe(gulp.dest(config.dest.scriptsDir));
-});
+require('./tasks/build.task')('default', gulp, {sma: smaParams, dest: config.dest});
+require('./tasks/watch.task')('watch', gulp, {sma: smaParams, dest: config.dest});
 
-gulp.task('include', () => {
-    return gulp.src('./src/include/*.inc')
-        .pipe(gulp.dest(config.dest.includeDir));
-});
-
-gulp.task('assets', () => {
-    return gulp.src('./assets/**/*')
-        .pipe(gulp.dest(config.dest.dir));
-});
-
-gulp.task('plugins:reapi', () => {
-    return gulp.src('./src/scripts/**/*.sma')
-        .pipe(sma(reapiSmaParams))
-        .pipe(flatten())
-        .pipe(gulp.dest(config.reapi.dest.scriptsDir));
-});
-
-gulp.task('include:reapi', () => {
-    return gulp.src('./src/include/*.inc')
-        .pipe(gulp.dest(config.reapi.dest.includeDir));
-});
-
-gulp.task('assets:reapi', () => {
-    return gulp.src('./assets/**/*')
-        .pipe(gulp.dest(config.reapi.dest.dir));
-});
-
-gulp.task('plugins:watch', () => {
-    return watch('./src/scripts/**/*.sma', {ignoreInitial: false})
-        .pipe(sma(smaParams))
-        .pipe(flatten())
-        .pipe(gulp.dest(config.dest.scriptsDir));
-});
-
-gulp.task('include:watch', () => {
-    return watch('./src/include/*.inc', {ignoreInitial: false})
-        .pipe(gulp.dest(config.dest.includeDir));
-});
-
-gulp.task('assets:watch', () => {
-    return watch('./assets/**/*', {ignoreInitial: false})
-        .pipe(gulp.dest(config.dest.dir));
-});
-
-gulp.task('default', ['plugins', 'plugins', 'include', 'assets']);
-gulp.task('reapi', ['plugins:reapi', 'plugins:reapi', 'include:reapi', 'assets:reapi']);
-gulp.task('watch', ['plugins:watch', 'include:watch', 'assets:watch']);
+require('./tasks/build.task')('reapi', gulp, {sma: reapiSmaParams, dest: config.reapi.dest});
