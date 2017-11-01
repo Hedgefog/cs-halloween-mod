0fd5bacb3014d6faea06fb702ce58e44225c0ae7
Yatskevych Egor <yatskevychegor@hotmail.com>
2017-11-01 13:32:06 +0200

    updated gitignore. added reapi gulp task. added reapi includes. gulp task refactoring. updated gulp-sma.

--- config.js
+++ config.js
@@ -6,11 +6,28 @@ const DIST_DIR = userConfig
     ? 'D:/Steam/steamapps/common/Half-Life/cstrike'
     : path.join(__dirname, 'dist');
 
+const REAPI_DIST_DIR = path.join(__dirname, 'dist_reapi');
+
 module.exports = {
-    distDir: DIST_DIR,
-    compiler: path.join(__dirname, 'compiler/amxxpc'),
-    includeDir: path.join(__dirname, 'src/include'),
-    pluginsDir: path.join(DIST_DIR, 'addons/amxmodx/plugins'),
-    scriptsDir: path.join(DIST_DIR, 'addons/amxmodx/scripting'),
-    includeDestDir: path.join(DIST_DIR, 'addons/amxmodx/scripting/include')
+    compiler: {
+        executable: path.join(__dirname, 'compiler/amxxpc')
+    },
+    project: {
+        includeDir: path.join(__dirname, 'src/include'),
+    },
+    dest: {
+        dir: DIST_DIR,
+        includeDir: path.join(DIST_DIR, 'addons/amxmodx/scripting/include'),
+        pluginsDir: path.join(DIST_DIR, 'addons/amxmodx/plugins'),
+        scriptsDir: path.join(DIST_DIR, 'addons/amxmodx/scripting')
+    },
+    reapi: {
+        includeDir: path.join(__dirname, 'reapi/include'),
+        dest: {
+            dir: REAPI_DIST_DIR,
+            includeDir: path.join(REAPI_DIST_DIR, 'addons/amxmodx/scripting/include'),
+            pluginsDir: path.join(REAPI_DIST_DIR, 'addons/amxmodx/plugins'),
+            scriptsDir: path.join(REAPI_DIST_DIR, 'addons/amxmodx/scripting')
+        }
+    }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -6,44 +6,71 @@ const flatten = require('gulp-flatten');
 const config = require('./config');
 
 const smaParams = {
-    compiler: config.compiler,
-    dest: config.pluginsDir,
-    includeDir: config.includeDir
+    compiler: config.compiler.executable,
+    dest: config.dest.pluginsDir,
+    includeDir: config.project.includeDir
+};
+
+const reapiSmaParams = {
+    compiler: config.compiler.executable,
+    includeDir: [
+        config.reapi.includeDir,
+        config.project.includeDir
+    ],
+    dest: config.reapi.dest.pluginsDir
 };
 
 gulp.task('plugins', () => {
     return gulp.src('./src/scripts/**/*.sma')
         .pipe(sma(smaParams))
         .pipe(flatten())
-        .pipe(gulp.dest(config.scriptsDir));
+        .pipe(gulp.dest(config.dest.scriptsDir));
 });
 
 gulp.task('include', () => {
     return gulp.src('./src/include/*.inc')
-        .pipe(gulp.dest(config.includeDestDir));
+        .pipe(gulp.dest(config.dest.includeDir));
 });
 
 gulp.task('assets', () => {
     return gulp.src('./assets/**/*')
-        .pipe(gulp.dest(config.distDir));
+        .pipe(gulp.dest(config.dest.dir));
+});
+
+gulp.task('plugins:reapi', () => {
+    return gulp.src('./src/scripts/**/*.sma')
+        .pipe(sma(reapiSmaParams))
+        .pipe(flatten())
+        .pipe(gulp.dest(config.reapi.dest.scriptsDir));
+});
+
+gulp.task('include:reapi', () => {
+    return gulp.src('./src/include/*.inc')
+        .pipe(gulp.dest(config.reapi.dest.includeDir));
+});
+
+gulp.task('assets:reapi', () => {
+    return gulp.src('./assets/**/*')
+        .pipe(gulp.dest(config.reapi.dest.dir));
 });
 
 gulp.task('plugins:watch', () => {
     return watch('./src/scripts/**/*.sma', {ignoreInitial: false})
         .pipe(sma(smaParams))
         .pipe(flatten())
-        .pipe(gulp.dest(config.scriptsDir));
+        .pipe(gulp.dest(config.dest.scriptsDir));
 });
 
 gulp.task('include:watch', () => {
     return watch('./src/include/*.inc', {ignoreInitial: false})
-        .pipe(gulp.dest(config.includeDestDir));
+        .pipe(gulp.dest(config.dest.includeDir));
 });
 
 gulp.task('assets:watch', () => {
     return watch('./assets/**/*', {ignoreInitial: false})
-        .pipe(gulp.dest(config.distDir));
+        .pipe(gulp.dest(config.dest.dir));
 });
 
-gulp.task('default', ['plugins', 'include', 'assets']);
+gulp.task('default', ['plugins', 'plugins', 'include', 'assets']);
+gulp.task('reapi', ['plugins:reapi', 'plugins:reapi', 'include:reapi', 'assets:reapi']);
 gulp.task('watch', ['plugins:watch', 'include:watch', 'assets:watch']);
