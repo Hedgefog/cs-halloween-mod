10f656f7bd7f6da8a3646d1ccd8477f70a74576e
Yatskevych Egor <hedgefog@mail.ru>
2017-11-01 19:23:35 +0200

    refactoring for gulp tasks. added package bundles tasks. updated gitignore.

--- config.js
+++ config.js
@@ -2,12 +2,6 @@ const path = require('path');
 
 const userConfig = false;
 
-const DIST_DIR = userConfig
-    ? 'D:/Steam/steamapps/common/Half-Life/cstrike'
-    : path.join(__dirname, 'dist');
-
-const REAPI_DIST_DIR = path.join(__dirname, 'dist_reapi');
-
 module.exports = {
     compiler: {
         executable: path.join(__dirname, 'compiler/amxxpc')
@@ -15,19 +9,15 @@ module.exports = {
     project: {
         includeDir: path.join(__dirname, 'src/include'),
     },
-    dest: {
-        dir: DIST_DIR,
-        includeDir: path.join(DIST_DIR, 'addons/amxmodx/scripting/include'),
-        pluginsDir: path.join(DIST_DIR, 'addons/amxmodx/plugins'),
-        scriptsDir: path.join(DIST_DIR, 'addons/amxmodx/scripting')
-    },
-    reapi: {
-        includeDir: path.join(__dirname, 'reapi/include'),
-        dest: {
-            dir: REAPI_DIST_DIR,
-            includeDir: path.join(REAPI_DIST_DIR, 'addons/amxmodx/scripting/include'),
-            pluginsDir: path.join(REAPI_DIST_DIR, 'addons/amxmodx/plugins'),
-            scriptsDir: path.join(REAPI_DIST_DIR, 'addons/amxmodx/scripting')
+    build: {
+        default: {
+            destDir: path.join(__dirname, 'dist')
+        },
+        game: {
+            destDir: 'D:/Steam/steamapps/common/Half-Life/cstrike'
+        },
+        reapi: {
+            destDir: path.join(__dirname, 'dist_reapi')
         }
     }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,128 @@
 const gulp = require('gulp');
-const watch = require('gulp-watch');
-const sma = require('gulp-sma');
-const flatten = require('gulp-flatten');
+const zip = require('gulp-zip');
+
+const path = require('path');
+
+const buildTaskFactory = require('./build-task-factory');
 
 const config = require('./config');
+const package = require('./package.json');
 
-const smaParams = {
+const resolveArchiveName = (sufix) => `hwn-${package.version.replace(/\./g, '')}-${sufix}.zip`;
+const resolveBundledDir = (name) => `bundles/${name}`;
+const resolveThirdparty = (relativepPath) => path.join(__dirname, 'thirdparty', relativepPath);
+const resolveDestConfig = (destDir) => ({
+    dir: destDir,
+    includeDir: path.join(destDir, 'addons/amxmodx/scripting/include'),
+    pluginsDir: path.join(destDir, 'addons/amxmodx/plugins'),
+    scriptsDir: path.join(destDir, 'addons/amxmodx/scripting')
+});
+
+const defaultDestConfig = resolveDestConfig(config.build.default.destDir);
+const gameDestConfig = resolveDestConfig(config.build.game.destDir);
+
+const defaultSmaConfig = {
     compiler: config.compiler.executable,
-    dest: config.dest.pluginsDir,
+    dest: defaultDestConfig.pluginsDir,
     includeDir: config.project.includeDir
 };
 
-const reapiSmaParams = {
+const gameSmaConfig = {
     compiler: config.compiler.executable,
-    includeDir: [
-        config.reapi.includeDir,
-        config.project.includeDir
-    ],
-    dest: config.reapi.dest.pluginsDir
+    dest: gameDestConfig.pluginsDir,
+    includeDir: config.project.includeDir
 };
 
-require('./tasks/build.task')('default', gulp, {sma: smaParams, dest: config.dest});
-require('./tasks/watch.task')('watch', gulp, {sma: smaParams, dest: config.dest});
+buildTaskFactory('build', {
+    smaConfig: defaultSmaConfig,
+    dest: defaultDestConfig
+});
 
-require('./tasks/build.task')('reapi', gulp, {sma: reapiSmaParams, dest: config.reapi.dest});
+buildTaskFactory('build:reapi', () => {
+    const destConfig = resolveDestConfig(config.build.reapi.destDir);
+
+    return {
+        smaConfig: {
+            compiler: config.compiler.executable,
+            dest: destConfig.pluginsDir,
+            includeDir: [
+                resolveThirdparty('reapi/addons/amxmodx/scripting/include'),
+                config.project.includeDir
+            ]
+        },
+        dest: destConfig,
+        tasks: {
+            assets: false
+        }
+    };
+});
+
+buildTaskFactory('watch', {
+    smaConfig: defaultSmaConfig,
+    dest: defaultDestConfig,
+    watch: true
+});
+
+buildTaskFactory('build-game', {
+    smaConfig: gameSmaConfig,
+    dest: gameDestConfig,
+    extraTasks: {
+        roundControl: () => gulp.src(resolveThirdparty('round-control') + '/**')
+            .pipe(gulp.dest(config.build.default.destDir))
+    }
+});
+
+buildTaskFactory('watch-game', {
+    smaConfig: gameSmaConfig,
+    dest: defaultDestConfig,
+    watch: true
+});
+
+gulp.task('pack:alliedmods', () => {
+    const distDir = config.build.default.destDir;
+    const buildDir = resolveBundledDir('alliedmods');
+
+    gulp.src([
+        distDir + '/**',
+        '!' + distDir + '/addons{,/**}',
+    ])
+        .pipe(zip(resolveArchiveName('resources')))
+        .pipe(gulp.dest(buildDir));
+
+    gulp.src([
+        distDir + '/addons{,/**}',
+        '!' + distDir + '/addons/amxmodx/plugins{,/**}',
+        '!' + distDir + '/addons/amxmodx/modules{,/**}',
+    ])
+        .pipe(zip(resolveArchiveName('addons')))
+        .pipe(gulp.dest(buildDir));
+});
+
+gulp.task('pack:full', () => {
+    const distDir = config.build.default.destDir;
+    const reapiDistDir = config.build.default.destDir;
+    const buildDir = resolveBundledDir('full');
+
+    gulp.src([
+        distDir + '/**',
+        '!' + distDir + '/addons{,/**}',
+    ])
+        .pipe(zip(resolveArchiveName('resources')))
+        .pipe(gulp.dest(buildDir));
+
+    gulp.src([
+        distDir + '/addons{,/**}',
+        resolveThirdparty('round-control') + '/**'
+    ])
+        .pipe(zip(resolveArchiveName('addons')))
+        .pipe(gulp.dest(buildDir));
+
+    gulp.src([
+        reapiDistDir + '/addons{,/**}',
+    ])
+        .pipe(zip(resolveArchiveName('addons-reapi')))
+        .pipe(gulp.dest(buildDir));
+});
+
+gulp.task('pack', ['pack:alliedmods', 'pack:full']);
+gulp.task('default', ['build', 'build:reapi']);
