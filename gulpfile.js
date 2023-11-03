099f3c8b95b70d7388233b3db521cb6aef42f11b
Hedgehog Fog <hedgehogfogtv@gmail.com>
2023-11-03 04:02:27 +0200

    update builder

--- gulpfile.js
+++ gulpfile.js
@@ -6,83 +6,129 @@ const file = require('gulp-file');
 const merge2 = require('merge2');
 
 const package = require('./package.json');
-const generateReadme = require('./helpers/bundle-readme.generator');
 
+const README_INDENT = '    ';
+const README_INSTALLATION_TEXT = 'Extract addons and resources to cstrike folder';
 const WORK_DIR = process.cwd();
 const DIST_DIR = path.join(WORK_DIR, './dist');
+const DIST_BUILD_DIR = path.join(DIST_DIR, 'build');
 const BUILD_DIR = path.join(DIST_DIR, 'bundles');
-const VANILLA_DIST_DIR = path.join(DIST_DIR, 'vanilla');
-const REAPI_DIST_DIR = path.join(DIST_DIR, 'reapi');
+const BUILD_METADATA_FILE = path.join(DIST_DIR, 'bundles', 'files.json');
+const SDK_DIR = path.join(WORK_DIR, 'sdk');
+const ASSETS_DIR = path.join(WORK_DIR, 'assets');
 
-if (!fs.existsSync(REAPI_DIST_DIR)) {
-    throw new Error('Build ReAPI project before packing');
+if (!fs.existsSync(DIST_BUILD_DIR)) {
+    throw new Error('Build project before packing');
+}
+
+function isDirEmpty(dir) {
+    if (!fs.existsSync(dir)) {
+        return true;
+    }
+
+    const files = fs.readdirSync(dir);
+
+    return !files.length;
+}
+
+function generateReadme(files) {
+    const filesArr = Object.keys(files).map(key => {
+        const { name, description } = files[key];
+        return { key, name, description };
+    });
+
+    const fileMaxLength = Math.max(
+        ...filesArr.map(file => file.name.length)
+    );
+
+    const resolvedFiles = filesArr.map(file => {
+        const { name, description } = file;
+
+        if (!description) {
+            return;
+        }
+
+        const spacesNum = Math.max(fileMaxLength - name.length, 0);
+        const spaces = ' '.repeat(spacesNum);
+
+        return `${README_INDENT}${name}${spaces} - ${description}`
+    }).filter(Boolean).join('\n');
+
+    return [
+        '[INSTALLATION]',
+        `${README_INDENT}${README_INSTALLATION_TEXT}`,
+        '',
+        '[FILES]',
+        resolvedFiles
+    ].join('\n');
 }
 
 const resolveArchiveName = (sufix) => `${package.name}-${package.version.replace(/\./g, '')}-${sufix}.zip`;
 
 const FILES = {
-    bundleArchive: resolveArchiveName('bundle'),
     srcArchive: resolveArchiveName('addons-src'),
-    addonsVanillaArchive: resolveArchiveName('addons'),
-    addonsReapiArchive: resolveArchiveName('addons-reapi'),
-    resourcesArchive: resolveArchiveName('resources'),
-    sdkArchive: resolveArchiveName('sdk'),
-    readme: 'README.TXT'
+    buildArchive: resolveArchiveName('addons-build'),
+    resourcesArchive: !isDirEmpty(ASSETS_DIR) ? resolveArchiveName('resources') : null,
+    sdkArchive: !isDirEmpty(SDK_DIR) ? resolveArchiveName('sdk') : null,
+    readme: 'README.TXT',
+    bundleArchive: resolveArchiveName('bundle')
 };
 
 const BUNDLE_FILES = [
-    { name: FILES.addonsVanillaArchive, description: 'compiled plugins and source code' },
-    { name: FILES.addonsReapiArchive, description: 'compiled plugins for reapi and source code' },
-    { name: FILES.resourcesArchive, description: 'mod resources' },
-    { name: FILES.sdkArchive, description: 'mod sdk' }
+    { name: FILES.buildArchive, description: 'compiled plugins and source code' }
 ];
 
-gulp.task('pack:vanilla:bundles', () => {
+if (FILES.resourcesArchive) {
+    BUNDLE_FILES.push({ name: FILES.resourcesArchive, description: 'mod resources' });
+}
+
+if (FILES.sdkArchive) {
+    BUNDLE_FILES.push({ name: FILES.sdkArchive, description: 'mod sdk' });
+}
+
+gulp.task('pack:bundles', () => {
     const dirPatterns = {
-        all: VANILLA_DIST_DIR + '/**',
-        addons: VANILLA_DIST_DIR + '/addons{,/**}',
-        plugins: VANILLA_DIST_DIR + '/addons/amxmodx/plugins{,/**}',
-        modules: VANILLA_DIST_DIR + '/addons/amxmodx/modules{,/**}',
-        roundControl: path.join(WORK_DIR, '.thirdparty/round-control/roundcontrol_2.2/**'),
-        sdk: WORK_DIR + '/sdk/**'
+        all: DIST_BUILD_DIR + '/**',
+        addons: DIST_BUILD_DIR + '/addons{,/**}',
+        plugins: DIST_BUILD_DIR + '/addons/amxmodx/plugins{,/**}',
+        modules: DIST_BUILD_DIR + '/addons/amxmodx/modules{,/**}',
+        sdk: SDK_DIR + '/**'
     };
 
+    const zipTasks = [
+        (
+            gulp
+                .src([dirPatterns.addons, '!' + dirPatterns.plugins, '!' + dirPatterns.modules])
+                .pipe(zip(FILES.srcArchive))
+        ),
+        (
+            gulp
+                .src([dirPatterns.addons])
+                .pipe(zip(FILES.buildArchive))
+        )
+    ];
+
+    if (FILES.resourcesArchive) {
+        zipTasks.push(
+            gulp
+                .src([dirPatterns.all, '!' + dirPatterns.addons])
+                .pipe(zip(FILES.resourcesArchive))
+        );
+    }
+
+    if(FILES.sdkArchive) {
+        zipTasks.push(
+            gulp
+                .src([dirPatterns.sdk])
+                .pipe(zip(FILES.sdkArchive))
+        );
+    }
+
     return merge2([
-        gulp.src([dirPatterns.addons, '!' + dirPatterns.plugins, '!' + dirPatterns.modules])
-            .pipe(zip(FILES.srcArchive)),
-        gulp.src([dirPatterns.addons, dirPatterns.roundControl])
-            .pipe(zip(FILES.addonsVanillaArchive)),
-        gulp.src([dirPatterns.all, '!' + dirPatterns.addons])
-            .pipe(zip(FILES.resourcesArchive)),
-        gulp.src([dirPatterns.sdk])
-            .pipe(zip(FILES.sdkArchive)),
-        file(FILES.readme, generateReadme(BUNDLE_FILES), {src: true})
+        ...zipTasks,
+        file(FILES.readme, generateReadme(BUNDLE_FILES), { src: true })
     ]).pipe(gulp.dest(BUILD_DIR));
 });
-
-gulp.task('pack:reapi:bundles', () => {
-    const dirPatterns = {
-        all: REAPI_DIST_DIR + '/**',
-        addons: REAPI_DIST_DIR + '/addons{,/**}',
-        plugins: REAPI_DIST_DIR + '/addons/amxmodx/plugins{,/**}',
-        modules: REAPI_DIST_DIR + '/addons/amxmodx/modules{,/**}',
-        sdk: WORK_DIR + '/sdk/**'
-    };
-
-    return merge2([
-        gulp.src([dirPatterns.addons, '!' + dirPatterns.plugins, '!' + dirPatterns.modules])
-            .pipe(zip(FILES.srcArchive)),
-        gulp.src([dirPatterns.addons])
-            .pipe(zip(FILES.addonsReapiArchive)),
-        gulp.src([dirPatterns.all, '!' + dirPatterns.addons])
-            .pipe(zip(FILES.resourcesArchive)),
-        gulp.src([dirPatterns.sdk])
-            .pipe(zip(FILES.sdkArchive)),
-        file(FILES.readme, generateReadme(BUNDLE_FILES), {src: true})
-    ]).pipe(gulp.dest(BUILD_DIR));
-});
-
-gulp.task('pack:bundles', gulp.series('pack:reapi:bundles', 'pack:vanilla:bundles'));
 
 gulp.task('pack:full', () => {
     const bundleFiles = BUNDLE_FILES.map(file => path.join(BUILD_DIR, file.name));
@@ -90,6 +136,10 @@ gulp.task('pack:full', () => {
     return gulp.src(bundleFiles)
         .pipe(zip(FILES.bundleArchive))
         .pipe(gulp.dest(BUILD_DIR))
-})
+});
 
-gulp.task('default', gulp.series('pack:bundles', 'pack:full'));
+gulp.task('pack:metadata', cb => {
+    fs.writeFile(BUILD_METADATA_FILE, JSON.stringify(FILES), cb);
+});
+
+gulp.task('default', gulp.series('pack:bundles', 'pack:full', 'pack:metadata'));
